5a8c07b7f3835e030346f03ab33b64b2c86fd7b6
mrmalhotra <deepakmalhotra2070@gmail.com>
2018-01-28 02:54:52 +0530

    fixed bug

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-mongoose.connect(config.production,(err)=>{
+mongoose.connect(config.development,(err)=>{
   if(!err)
     console.log('db connected..!')
 })
